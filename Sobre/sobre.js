877c88515ca4e1a910ad9c4117665a601290b469
Adelmo <adelmopotenca@hotmail.com>
2024-06-21 16:55:04 -0300

    commit

--- Sobre/sobre.js
+++ Sobre/sobre.js
@@ -1,7 +1,9 @@
-window.revelar = ScrollReveal({ reset: true });
 
 
-revelar.reveal('.efeito-div', {
+window.revelar = ScrollReveal({reset:true});
+
+
+revelar.reveal('.efeito-div1', {
     duration: 3000,
     distance: '150px'
 });
